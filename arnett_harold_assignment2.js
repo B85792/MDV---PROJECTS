1d3fd13aec3aff5dedc3e6f0771636d6813e7edd
harold arnett <lynn72@fullsail.edu.com>
2013-02-14 18:37:15 -0500

    almost done

--- arnett_harold_assignment2.js
+++ arnett_harold_assignment2.js
@@ -5,4 +5,113 @@
 // SDI 1302
 // HOME REMODEL
 
+// My Variables
 
+var houseProject = "remodel";
+var project = "kitchen cabinets";
+var material = "wood used";
+var Months = 2;
+var Sheets = 9;
+var typeOfDoors = "flat panel doors";
+var hardwareFinish = ("rubbed bronze");
+var Handles = 25;
+var Hinges = 50;
+
+// My Procedure
+
+var division = function(x,y) {
+    var outcome = project/9;
+    
+    console.log("I have enough materials to complete this project!");
+};
+
+var material = true;
+
+ if ( material === false) { 
+    console.log("I will have to go back and buy more wood!");
+    } else {
+    console.log("I will have enough wood for the project!");
+};
+
+
+//Boolean   
+var p = project, q = "wood", r = 9;
+
+// Given: (p ∧ q) ∨ r
+    if ((p && q) || r) {
+        console.log("The expression is true");
+    } else {
+        console.log("The expression is false");
+};   
+
+ // Array
+var wood1 = "oak",
+	wood2 = "birch",
+	wood3 = "maple";
+	  
+var woods =[
+"Oak",
+"Birch",
+"Maple"
+];
+ console.log( woods.length + " types of woods to choose from!");
+
+
+var adjectives =[
+ 	 o = " is a hard wood!",
+     b = " is a hard wood!",
+     m = " is a soft wood!"
+]; 
+	console.log(woods[0] + o );
+	console.log(woods[1] + b );
+	console.log(woods[2] + m );
+
+ for(var i=0, j=woods.length; i < j; i++) {
+	console.log(woods[i]);
+};  
+  
+ if ( woods[0] + o === false) { 
+    console.log("I will have the right wood for the job!");
+    } else {
+    console.log("I will have to choose a better wood to use!");
+    
+// Number
+
+var doors = 25
+var handles = 25
+var hinges = 50
+
+console.log(handles + hinges);
+
+// While Loop
+var oak = 9
+	while(oak>0){
+	console.log(oak + "sheets of material used!");
+	oak--;
+	
+};
+console.log("I have no materials left!");
+ 
+};
+    
+// String
+var a = true,
+	b = false,
+	c = "is ",
+	d = "now done!";
+
+var hrp = "My kitchen remodel ";{
+	var jobDone = hrp +  c  +  d;
+	console.log(hrp  +  c   + d);
+ };   
+    
+// Return Values
+    
+    
+    
+    
+    
+    
+    
+    
+    
